e87b51a87c81bed24bfee4b3dd79e8bf06c10013
ojogbon <40061054+ojogbon@users.noreply.github.com>
2020-12-24 13:16:41 -0800

    set project structure

--- index.php
+++ index.php
@@ -21,6 +21,27 @@
     <!-- ................Fonts...................... -->
       <!-- Trebuchet MS -->
       <link rel="stylesheet" href="fonts/Trebuchet MS/Trebuchet-MS.ttf">
+      <style>
+        .tutor-can-do-it {
+              display:none;
+        }
+
+        .tutor-can-do-it , .student-can-do-it{
+          padding: 2em;
+
+        }
+
+        .changePane {
+    margin-top: 10px;
+    border: 1px solid #fff;
+    padding: 2px;
+    background-color: #fff;
+    border-top-right-radius: 5px;
+    border-bottom-left-radius: 5px;
+    box-shadow: 1px 1px 1px 1px;
+    cursor: pointer;
+        }
+      </style>
 </head>
 
 
@@ -58,14 +79,14 @@
                  <div id="formCasing" class="container-fluid p-5">
                     <div id="formCasing-sm" class="container w-50 orange border border-danger" style="border-top-right-radius:50px; border-bottom-left-radius:50px">
                       
-                        <form action="login.php" method="GET" class="p-5 col-12 shadow">
+                        <form class="student-can-do-it" action="login.php" method="GET" class="p-5 col-12 shadow">
                           <div class="formContent">
                           
                           <div class="container imgLoginClose w-25 pb-3 text-center">
                             <span class="text-center close-login"><img height= 70px; width= 70px; style="border: 1px solid orange; border-radius: 70%;" src="img/showcase.jpg"></span>
                           </div>
 
-                          <h3 class="text-center text-danger pb-3">Login to your account</h3>
+                          <h3 class="text-center text-danger pb-3">Student Login to your account</h3>
                           <p class="lead text-center text-dark">Enter your email and password to access your account</p>
                           <br>
 
@@ -87,7 +108,54 @@
                                 </div>
                                     
                                 <div class="col-md-6">
-                                    <a href="register-as-tutor.php" class="text-left text-dark">Register here!</a>
+                                    <a href="./o.pages/get-a-tutor.php" class="text-left text-dark">Register here!</a>
+                                </div>
+                            </div>
+
+                            <div id="formFooter" class="row container">
+                                <div class="col-md-6">
+                                    <a class="text-dark" href="../Academia O/o.pages/reset-password.php">Forgot password?</a>
+                                </div>
+                                    
+                                <div class="col-md-6">
+                                    <a class="text-left text-dark" href="../Academia O/o.pages/help.php">Can't log in?</a>
+                                </div>
+
+                            </div>
+                            <span class="changePane to-tutor">Tutor Pane!</span>
+                          </div>
+                        </form>
+
+                        <form class="tutor-can-do-it" action="login.php" method="GET" class="p-5 col-12 shadow">
+                          <div class="formContent">
+                          
+                          <div class="container imgLoginClose w-25 pb-3 text-center">
+                            <span class="text-center close-login"><img height= 70px; width= 70px; style="border: 1px solid orange; border-radius: 70%;" src="img/showcase.jpg"></span>
+                          </div>
+
+                          <h3 class="text-center text-danger pb-3">Tutor Login to your account</h3>
+                          <p class="lead text-center text-dark">Enter your email and password to access your account</p>
+                          <br>
+
+                            <div class="form-group">
+                                <input type="email" id="email" class="form-control shadow" name="con_email" placeholder="Email">
+                            </div>
+                            <br>
+                            
+                            <div class="form-group shadow">
+                                <input type="text" id="password" class="form-control" name="password" placeholder="Password">
+                            </div>
+                            <br>
+
+                            <input class="btn btn-lg btn-outline-danger btn-block shadow" type="submit" name="login_button" value="Log in">
+
+                            <div id="formFooter" class="row my-3 container">
+                                <div class="col-md-6">
+                                    No account yet?
+                                </div>
+                                    
+                                <div class="col-md-6">
+                                    <a href="./o.pages/become-a-tutor.php" class="text-left text-dark">Register here!</a>
                                 </div>
                             </div>
 
@@ -100,7 +168,7 @@
                                     <a class="text-left text-dark" href="../Academia O/o.pages/help.php">Can't log in?</a>
                                 </div>
                             </div>
-                          
+                            <span class="changePane to-student">Student Pane!</span>
                           </div>
                         </form>
 
@@ -337,5 +405,24 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/index.js"></script>
 
+<script>
+
+function doTheChangeOver (){
+    // alert (2323);
+     
+    document.querySelector(".to-tutor").addEventListener('click', event => {
+      document.querySelector(".student-can-do-it").style = "display : none";
+      document.querySelector(".tutor-can-do-it").style = "display : block";
+    });
+
+    document.querySelector(".to-student").addEventListener('click', event => {
+      document.querySelector(".tutor-can-do-it").style = "display : none";
+      document.querySelector(".student-can-do-it").style = "display : block";
+    });
+}
+
+doTheChangeOver();
+
+</script>
 </body>
 </html>
