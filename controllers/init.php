7463c00d34bc0812fc7afe928835bacbefb0d104
ojogbon <40061054+ojogbon@users.noreply.github.com>
2021-01-18 10:52:22 -0800

    edit init file for heroku run task file

--- controllers/init.php
+++ controllers/init.php
@@ -7,8 +7,9 @@ session_start();
 $dev_path = "pro";
 $companyname = "Academia";
 
-$parent_path = $dev_path == "dev" ? "Academia/" : "";
+$parent_path = $dev_path == "dev" ? "Academia/" : "/";
 
 
      include ($_SERVER['DOCUMENT_ROOT']."/". $parent_path.'models/MainModel.php');
+     echo $_SERVER['DOCUMENT_ROOT']."/". $parent_path.'models/MainModel.php';
      $mainModel = new MainModel();
